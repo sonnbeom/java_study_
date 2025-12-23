bad924bbf228b11a18b753fa25cc3b55f3e047d5
soony <dhsmf4245@gmail.com>
2025-12-23 20:32:58 +0900

    TIL

---
+++ src/generic/ex1/BoxMainOne.java
@@ -0,0 +1,15 @@
+package generic.ex1;
+
+public class BoxMainOne {
+    public static void main(String[] args) {
+        IntegerBox integerBox = new IntegerBox();
+        integerBox.setValue(10);
+        Integer integer = integerBox.getValue();
+        System.out.println("integer = " + integer);
+
+        StringBox stringBox = new StringBox();
+        stringBox.setValue("hello");
+        String str = stringBox.getValue();
+        System.out.println("str = " + str);
+    }
+}
