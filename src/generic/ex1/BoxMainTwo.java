d4a386d24b822a10fcd1625b1ad4ae9a328a633e
soony <dhsmf4245@gmail.com>
2025-12-24 20:06:41 +0900

    TIL

--- src/generic/ex1/BoxMainTwo.java
+++ src/generic/ex1/BoxMainTwo.java
@@ -2,10 +2,19 @@ package generic.ex1;
 
 public class BoxMainTwo {
     public static void main(String[] args) {
-        ObjectBox objectBox = new ObjectBox();
-        objectBox.setValue(10);
-        Integer integer = (Integer) objectBox.getValue();
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.setValue(10);
+        Integer integer = (Integer) integerBox.getValue();
         System.out.println("Integer = " + integer);
 
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.setValue("hello");
+        String str = (String) stringBox.getValue();
+        System.out.println("str = " + str);
+
+        // 잘못된 타입의 인수 전달시
+        integerBox.setValue("문자를 실수로 넣어봅시다.");
+        Integer result = (Integer) integerBox.getValue();
+        System.out.println("result = " + result);
     }
 }
