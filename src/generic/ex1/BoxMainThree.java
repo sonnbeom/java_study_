1369fa2997c2d309775905cc624022d84285927c
soony <dhsmf4245@gmail.com>
2025-12-26 13:27:23 +0900

    TIL

--- src/generic/ex1/BoxMainThree.java
+++ src/generic/ex1/BoxMainThree.java
@@ -3,6 +3,7 @@ package generic.ex1;
 public class BoxMainThree {
     public static void main(String[] args) {
 
+        // 타입 매개변수 (=T)에 타입 인자(아래 예시를 참고하자면 Integer, String, Double이다)를 전달해서 사용할 타입을 결정한다.
         GenericBox<Integer> integerBox = new GenericBox<>(); // 생성시점에 T의 타입 결정
         integerBox.setValue(1);
         Integer value = integerBox.getValue();
@@ -17,6 +18,7 @@ public class BoxMainThree {
         doubleGenericBox.setValue(10.5);
         Double aDouble = doubleGenericBox.getValue();
         System.out.println("double = " + aDouble);
+
 
     }
 }
---
+++ src/generic/test/ex3/AnimalHospitalMainV0.java
@@ -0,0 +1,21 @@
+package generic.test.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV0 {
+
+    public static void main(String[] args) {
+        DogHospital dogHospital = new DogHospital();
+        CatHospital catHospital = new CatHospital();
+
+        Dog dog = new Dog("멍멍1", 100);
+        Cat cat = new Cat("냥오이" , 300);
+
+        dogHospital.setAnimal(dog);
+        dogHospital.checkUp();;
+
+        catHospital.setAnimal(cat);
+        catHospital.checkUp();
+    }
+}
