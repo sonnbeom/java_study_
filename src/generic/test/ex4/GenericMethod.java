1ddadbc9c3c61bcd1dcaf97426f9ee54831ce5b6
soony <dhsmf4245@gmail.com>
2025-12-27 13:07:16 +0900

    TIL

---
+++ src/generic/test/ex4/GenericMethod.java
@@ -0,0 +1,22 @@
+package generic.test.ex4;
+
+public class GenericMethod {
+
+    public static Object objMethod(Object obj){
+        System.out.println("OBJ PRINT : " + obj);
+        return obj;
+    }
+
+    //반환 타입 앞에 제네릭 타입 매개변수를 적어주면 된다.
+    public static <T> T genericMethod(T t){
+        System.out.println("OBJ PRINT : " + t);
+        return t;
+    }
+
+
+    //제한을 둘 수도 있다.
+    public static <T extends Number> T numberMethod(T t){
+        System.out.println("OBJ PRINT : " + t);
+        return t;
+    }
+}
---
+++ src/generic/test/ex4/MethodMain1.java
@@ -0,0 +1,31 @@
+package generic.test.ex4;
+
+public class MethodMain1 {
+    public static void main(String[] args) {
+        Integer i = 10;
+        Object i1 = GenericMethod.objMethod(i);
+        Integer i2 = (Integer) GenericMethod.objMethod(i);
+
+        // 타입인자(Type Argument) 명시적 전달
+        System.out.println("명시적 타입 인자 전달");
+        Integer i3 = GenericMethod.<Integer>genericMethod(i);
+
+        Integer integer = GenericMethod.<Integer>numberMethod(10);
+        Double v = GenericMethod.<Double>numberMethod(10.0);
+
+        // 타입 추론 .<Integer>을 안 적어도 된다. 타입 정보를 보고 결정한다
+        Integer integer5 = GenericMethod.numberMethod(10);
+        Double v5 = GenericMethod.numberMethod(10.0);
+        /*
+        * 제네릭 타입
+        정의: GenericClass<T>
+        타입 인자 전달: 객체를 생성하는 시점
+        예) new GenericClass<String>
+
+        제네릭 메서드
+        정의: <T> T genericMethod(T t)
+        타입 인자 전달: 메서드를 호출하는 시점
+        예) GenericMethod.<Integer>genericMethod(i)
+        * */
+    }
+}
