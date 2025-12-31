9d8d5c83df3b63c377dd13d4c7b447e53d79d806
soony <dhsmf4245@gmail.com>
2025-12-31 21:43:36 +0900

    TIL

---
+++ src/collection/array/ArrayMain1.java
@@ -0,0 +1,27 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain1 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        System.out.println(Arrays.toString(arr));
+
+        arr[2] = 10;
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("index 조회 :");
+        int value = 10;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "]: " + +arr[i]);
+            if (arr[i] == value){
+                System.out.println(value + " 찾음");
+                break;
+            }
+        }
+
+    }
+}
