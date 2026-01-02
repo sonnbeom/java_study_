384b4751ba5b8b4b61de933006e4dcf6362a05b3
soony <dhsmf4245@gmail.com>
2026-01-02 20:20:43 +0900

    TIL

---
+++ src/collection/list/BatchProcessor.java
@@ -0,0 +1,19 @@
+package collection.list;
+
+public class BatchProcessor {
+    private final MyList<Integer> list;
+
+    public BatchProcessor(MyList<Integer> list) {
+        this.list = list;
+    }
+
+    public void logic(int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) { //O(n)
+            list.add(0, i); //앞에 추가 링크드리스트: O(1) 어레이리스트: O(n)
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("크기 : " + size + "계산 시간: " + (endTime - startTime) + "ms");
+
+    }
+}
